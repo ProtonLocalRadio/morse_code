6fd1fb95dc2366fbd49f4fbb29c79c4bc437c0ee
ProtonLocalRadio <77429184+ProtonLocalRadio@users.noreply.github.com>
2021-01-13 23:11:24 -0700

    Add files via upload

---
+++ morse_code.ino
@@ -0,0 +1,897 @@
+//letters
+void A() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void B() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void C() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void D() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void E() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void f() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void G() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void H() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void I() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void J() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void K() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void L() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void M() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void N() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void O() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void P() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void Q() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void R() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void S() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void T() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void U() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void V() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void W() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void X() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void Y() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void Z() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void one() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void two() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void three() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void four() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void five() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void six() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void seven() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void eight() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void nine() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(125);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+void zero() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(375);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(125);
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("morse_code by Adan");
+  Serial.println("Please Input UPPER CASE text, f is LOWER CASE for translation into morse code");
+  Serial.println("WIP - Notes : punctuation DOES NOT EXIST, yet. f Is LOWER CASE, a bug :(. working on printing what is typed.");
+}
+
+void loop() {
+  if  (Serial.available() > 0)
+  {
+    char letter = Serial.read();
+
+    if (letter == 'A')
+    {
+      A();
+      delay(125);
+    }
+    else if (letter == 'B')
+    {
+      B();
+      delay(125);
+    }
+    else if (letter == 'C')
+    {
+      C();
+      delay(125);
+    }
+    else if (letter == 'D')
+    {
+      D();
+      delay(125);
+    }
+    else if (letter == 'E')
+    {
+      E();
+      delay(125);
+    }
+    else if (letter == 'f')
+    {
+      f();
+      delay(125);
+    }
+    else if (letter == 'G')
+    {
+      G();
+      delay(125);
+    }
+    else if (letter == 'H')
+    {
+      H();
+      delay(125);
+    }
+    else if (letter == 'I')
+    {
+      I();
+      delay(125);
+    }
+    else if (letter == 'J')
+    {
+      J();
+      delay(125);
+    }
+    else if (letter == 'K')
+    {
+      K();
+      delay(125);
+    }
+    else if (letter == 'L')
+    {
+      L();
+      delay(125);
+    }
+    else if (letter == 'M')
+    {
+      M();
+      delay(125);
+    }
+    else if (letter == 'N')
+    {
+      N();
+      delay(125);
+    }
+    else if (letter == 'O')
+    {
+      O();
+      delay(125);
+    }
+    else if (letter == 'P')
+    {
+      P();
+      delay(125);
+    }
+    else if (letter == 'Q')
+    {
+      Q();
+      delay(125);
+    }
+    else if (letter == 'R')
+    {
+      R();
+      delay(125);
+    }
+    else if (letter == 'S')
+    {
+      S();
+      delay(125);
+    }
+    else if (letter == 'T')
+    {
+      T();
+      delay(125);
+    }
+    else if (letter == 'U')
+    {
+      U();
+      delay(125);
+    }
+    else if (letter == 'V')
+    {
+      V();
+      delay(125);
+    }
+    else if (letter == 'W')
+    {
+      W();
+      delay(125);
+    }
+    else if (letter == 'X')
+    {
+      X();
+      delay(125);
+    }
+    else if (letter == 'Y')
+    {
+      Y();
+      delay(125);
+    }
+    else if (letter == 'Z')
+    {
+      Z();
+      delay(125);
+    }
+    else if (letter == ' ')
+    {
+      delay(875);
+    }
+    else if (letter == '1')
+    {
+      one();
+      delay(125);
+    }
+    else if (letter == '2')
+    {
+      two();
+      delay(125);
+    }
+    else if (letter == '3')
+    {
+      three();
+      delay(125);
+    }
+    else if (letter == '4')
+    {
+      four();
+      delay(125);
+    }
+    else if (letter == '5')
+    {
+      five();
+      delay(125);
+    }
+    else if (letter == '6')
+    {
+      six();
+      delay(125);
+    }
+    else if (letter == '7')
+    {
+      seven();
+      delay(125);
+    }
+    else if (letter == '8')
+    {
+      eight();
+      delay(125);
+    }
+    else if (letter == '9')
+    {
+      nine();
+      delay(125);
+    }
+    else if (letter == '0')
+    {
+      zero();
+      delay(125);
+    }
+  }
+}
